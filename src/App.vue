09e34cf5d167d8435667d8b3fd8eb842260814dd
Deepak Shukla <deepak2.shukla@paytm.com>
2019-06-25 01:11:11 +0530

    Intial project setup in vue

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+  <ppm-projectAccess></ppm-projectAccess>
+  <ppm-productAccess></ppm-productAccess>
+  <ppm-strategyAccess></ppm-strategyAccess>
+  </div>
+</template>
+
+<script>
+import projectAccess from './Components/projectAccess/projectAccess'
+import productAccess from './Components/productAccess/productAccess'
+import strategyAccess from './Components/strategyAccess/strategyAccess'
+export default {
+  components:{
+    'ppm-projectAccess':projectAccess,
+    'ppm-productAccess':productAccess,
+    'ppm-strategyAccess':strategyAccess
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Components/productAccess/productAccess.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="accordion" id="productAccessAccordian">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Product Access Required
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#productAccessAccordian">
+      <div class="card-body">
+        <form>
+  <div class="form-group">
+    <label for="projectLevelSelect">Project Level</label>
+    <select class="form-control" id="projectLevelSelect">
+      <option>Select Project Level</option>
+      <option>Organiztion</option>
+      <option>Department</option>
+      <option>Group</option>
+      <option>Team/Product</option>
+      <option>Project</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="grantDescription">Grant Description</label>
+    <input type="text" class="form-control" id="grantDescription" placeholder="Enter grant description">
+  </div>
+  <div class="form-group">
+    <label for="rorwgrants">RO/RW Grants</label>
+    <select class="form-control" id="rorwgrants">
+      <option>Select Grant Option </option>
+      <option>RO</option>
+      <option>RW</option>
+    </select>
+  </div>
+</form>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
